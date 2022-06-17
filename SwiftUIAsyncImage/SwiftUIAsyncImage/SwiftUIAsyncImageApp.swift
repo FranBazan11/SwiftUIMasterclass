6d464d3ea156c4ae9768fb6a0d58463b92de5276
Juan Francisco Bazan Carrizo <juanfrancisco.bazancarrizo@mercadolibre.com>
2022-06-16 22:02:01 -0300

    Restart app

---
+++ SwiftUIAsyncImage/SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  SwiftUIAsyncImage
+//
+//  Created by Juan Francisco Bazan Carrizo on 26/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
