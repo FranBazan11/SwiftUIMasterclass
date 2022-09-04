0471bcda10e774f4e589c9ff59dd04012e393341
Juan Francisco Bazan Carrizo <juanfrancisco.bazancarrizo@mercadolibre.com>
2022-09-03 22:36:51 -0300

    QRCodeView

---
+++ QRCodeView/QRCodeView/QRCodeViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeViewApp.swift
+//  QRCodeView
+//
+//  Created by Juan Francisco Bazan Carrizo on 19/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
