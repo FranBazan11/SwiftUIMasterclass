768f2a9819fd74d6f8416e2a3b8aa5716beab806
Juan Bazan Carrizo <fran.bazan11@icloud.com>
2022-11-12 02:10:23 -0300

    Initial app

---
+++ APP-TOUCHDOWN/Students/Touchdown/Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Juan Bazan Carrizo on 11/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
